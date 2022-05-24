ecf4432c537aaaf1ae1543d78830074a68bd7ed9
Taha K <15181973+bktaha@users.noreply.github.com>
2022-05-24 10:53:06 +0530

    move imports to method

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 # MOKSHA
 
-from pandas import read_csv
 from typing import List, Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
-from turicreate import load_images, load_model
-from turicreate import image_analysis
 
 app = FastAPI()
 BASE_S3_URL = 'https://mokshawd.s3.ap-south-1.amazonaws.com/'
@@ -28,11 +25,13 @@ def read_root():
 
 @app.post("/features", response_model=OutputListModel)
 def get_features(itmurl: InputImageURL):
+    from turicreate import load_images, image_analysis
     image_sframe = load_images(itmurl.imgur_url)
     deep_features_sframe = image_analysis.get_deep_features(image_sframe['image'], model_name='squeezenet_v1.1')
     return OutputListModel(img_features= list(deep_features_sframe[0]))
 
 def get_filenames(ref_id: List[int], model_name: str =  'rn'):
+    from pandas import read_csv
     img_paths = read_csv('allimages_{}_paths.csv'.format(model_name))
     img_paths.columns = ['ref_id', 'img_filename']
     img_paths['img_filename'] = img_paths['img_filename'].str.replace(' ', '+', regex=False)
@@ -40,6 +39,7 @@ def get_filenames(ref_id: List[int], model_name: str =  'rn'):
 
 @app.post("/similar", response_model=List[OutputSimilarModel])
 def get_similar(itmurl: InputImageURL):
+    from turicreate import load_images, load_model
     image_sframe = load_images(itmurl.imgur_url)
     sn_model = load_model('allimages_{}.model'.format(itmurl.model_name))
 
